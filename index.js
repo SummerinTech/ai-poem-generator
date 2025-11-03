9109a5c9ecff818e2cb6d56a8282d2b710071e3e
Summer <snrobinson13@gmail.com>
2025-11-02 21:18:58 -0800

    set up basic homepage and connect project to Netlify

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+async function showAIResponse(response) {
+	console.log(response.data);
+}
+
 const apiKey = import.meta.env.VITE_API_KEY;
 const prompt = "who was the first president of the United States?";
 const context =
 	"Please only respond with their first and last name and the year of their first term";
 const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+axios.get(apiUrl).then(showAIResponse);
