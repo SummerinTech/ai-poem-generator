57a6abe12249d94d6050f6b716a50168ee6112fc
Summer <snrobinson13@gmail.com>
2025-11-05 10:40:08 -0800

    clean up code and improve UI experience with blinking text

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+let instructionsInput = document.querySelector("#user-instructions");
 
 function showAIResponse(response) {
 	new Typewriter("#poem", {
@@ -7,17 +8,21 @@ function showAIResponse(response) {
 		delay: 1,
 		cursor: "",
 	});
+	instructionsInput.value = "";
 }
 
 function generatePoem(event) {
 	event.preventDefault();
 
-	let instructionsInput = document.querySelector("#user-instructions");
 	const apiKey = import.meta.env.VITE_API_KEY;
 	const prompt = `User instructions are: Generate a poem in English about ${instructionsInput.value}`;
 	const context =
 		"You are a romantic poem expert and love to write short poems. Your mission is to generate a four line poem and seperate each line with a <br />.Do not include a title. Make sure to follow the user instructions.";
 	const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+	let poemElement = document.querySelector("#poem");
+	poemElement.classList.remove("hidden");
+	poemElement.innerHTML = `<div class="blink">Generating poem about "${instructionsInput.value}" ⏳</div>`;
 
 	axios.get(apiUrl).then(showAIResponse);
 }
